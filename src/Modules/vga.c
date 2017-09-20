62885c8a19fee763eac55450f19b91a804885623
DogeProgrammer <panczerwonyser@wp.pl>
2017-09-20 14:01:45 +0200

    Added putchar

--- src/Modules/vga.c
+++ src/Modules/vga.c
@@ -1,23 +1,26 @@
 char* vga_buffer = (char*)0xb8000;
  
- void clear() {
- 	unsigned short j = 0;
+void clear() {
+ unsigned short j = 0;
  
- 	while(j < 80 * 25 * 2) {
- 		vga_buffer[j] = ' ';
- 		vga_buffer[j+1] = 0x07;
- 		j += 2;
- 	}
+ while(j < 80 * 25 * 2) {
+ 	putchar(' ', j);
+  j += 2;
  }
+}
  
- void puts(const char* string) {
- 	unsigned short i = 0;
- 	unsigned short j = 0;
+void putchar(char character, int pos) {
+ vga_buffer[pos] = character;
+ vga_buffer[pos+1] = 0x07;
+}
+
+void puts(const char* string) {
+ unsigned short i = 0;
+ unsigned short j = 0;
  
- 	while(string[i] != '\0') {
- 		vga_buffer[j] = string[i];
- 		vga_buffer[j+1] = 0x07;
- 		j += 2;
- 		i += 1;
- 	}
+ while(string[i] != '\0') {
+ 	putchar(string[i], j);
+ 	j += 2;
+ 	i += 1;
  }
+}
