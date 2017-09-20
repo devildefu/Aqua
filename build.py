aa1ede272b1580484510ce3f2630ebf0b5a00199
DogeProgrammer <panczerwonyser@wp.pl>
2017-09-20 14:13:38 +0200

    Added io.o

--- build.py
+++ build.py
@@ -4,7 +4,8 @@ commands = [
     "nasm -felf32 src/boot.asm -o build/boot.o",
     "gcc -c src/kernel.c -o build/kernel.o -std=c99 -O2 -Wall -Wextra -fno-builtin -nostdlib",
     "gcc -c src/Modules/vga.c -o ../build/vga.o -std=c99 -O2 -Wall -Wextra -fno-builtin -nostdlib",
-    "ld build/boot.o build/kernel.o build/vga.o -T linker.ld -o kernel -O2 -nostdlib"
+    "gcc -c src/Modules/io.c -o build/io.o -std=c99 -O2 -Wall -Wextra -fno-builtin -nostdlib",
+    "ld build/boot.o build/kernel.o build/vga.o build/io.c -T linker.ld -o kernel -O2 -nostdlib"
 ]
 
 for cmd in commands:
