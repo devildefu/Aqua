6cba6624b7c791eb9bfd03fc524de530d01a80ab
Szczepan Szpilczyński <szczepan2467@gmail.com>
2017-09-21 15:14:06 +0200

    Update build.py

--- build.py
+++ build.py
@@ -2,10 +2,10 @@ import os
 
 commands = [
     "nasm -felf32 src/boot.asm -o build/boot.o",
-    "gcc -c src/kernel.c -o build/kernel.o -std=c99 -O2 -Wall -Wextra -fno-builtin -nostdlib",
-    "gcc -c src/Modules/vga.c -o ../build/vga.o -std=c99 -O2 -Wall -Wextra -fno-builtin -nostdlib",
-    "gcc -c src/Modules/io.c -o build/io.o -std=c99 -O2 -Wall -Wextra -fno-builtin -nostdlib",
-    "ld build/boot.o build/kernel.o build/vga.o build/io.c -T linker.ld -o kernel -O2 -nostdlib"
+    "gcc -m32 -c src/Modules/vga.c -o build/vga.o -std=gnu99 -O2 -Wall -Wextra -fno-builtin -nostdlib",
+    "gcc -m32 -c src/kernel.c -o build/kernel.o -std=gnu99 -O2 -Wall -Wextra -fno-builtin -nostdlib",
+    "gcc -c src/Modules/io.c -o build/io.o -std=gnu99 -O2 -Wall -Wextra -fno-builtin -nostdlib",
+    "ld -m elf_i386 build/boot.o build/kernel.o build/vga.o -T linker.ld -o kernel -O2 -nostdlib"
 ]
 
 for cmd in commands:
