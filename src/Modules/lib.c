0a975d0cdcb52177115add4f875c6bc7506a6f69
Grzegorz Kociołek <georgekociolek@gmail.com>
2017-09-24 02:26:13 +0200

    Added system functions

--- src/Modules/lib.c
+++ src/Modules/lib.c
@@ -11,3 +11,30 @@ void atoi(char *str, int* a)
     }
     *a = k;
 }
+
+//////////
+///SYS////
+void* malloc(unsigned size); //<- Not now, maybe tomorrow
+void free(void* ptr); //<- Not now, maybe tomorrow
+
+int memcmp(const void* a, const void* b, unsigned int size) {
+    if(size>0) {
+        unsigned int goodbytes = 0;
+        for(int i = 0; i<size; i++) {
+            if(((const char*)a)[i] == ((const char*)b)[i]) goodbytes++;
+        }
+        if(goodbytes>=size) return 0;
+        if(((const char*)a)[0] > ((const char*)b)[0]) return 1;
+        if(((const char*)a)[0] < ((const char*)b)[0]) return -1;
+    }
+    return 1;
+}
+
+void* memcpy(void* destination, const void* from, unsigned int block_size) {
+    if(block_size>0) {
+        for(int i = 0; i<block_size; i++) {
+            ((char*)destination)[i] = ((const char*)from)[i];
+        }
+    }
+    return destination;
+}
