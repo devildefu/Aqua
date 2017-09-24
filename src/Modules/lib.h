c0f5d6210f3d528e303d7d36c9717c990df8b4e6
Grzegorz Kociołek <georgekociolek@gmail.com>
2017-09-24 02:20:33 +0200

    Added system functions

--- src/Modules/lib.h
+++ src/Modules/lib.h
@@ -3,4 +3,12 @@
 
 void atoi(char *str, int* a);
 
+///SYS////
+void* malloc(unsigned size); //<- Not now, maybe tomorrow
+void free(void* ptr); //<- Not now, maybe tomorrow
+
+int memcmp(const void* a, const void* b, unsigned int size);
+void* memcpy(void* destination, const void* from, unsigned int block_size);
+
+
 #endif
