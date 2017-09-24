1a44b8d7c1a80f7ede6258a3e4cca9f432d44376
Grzegorz Kociołek <georgekociolek@gmail.com>
2017-09-24 21:28:02 +0200

    Added outb

--- src/Modules/io.h
+++ src/Modules/io.h
@@ -10,4 +10,8 @@ static inline uint8_t inb(uint16_t port) {
 	return ret;
 }
 
+static inline void outb(uint16_t port, uint8_t val) {
+    asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
+}
+
 #endif
