5eef4e4a7f38127c148549088b0409a21ca54a0f
Grzegorz Kociołek <georgekociolek@gmail.com>
2017-09-24 16:35:25 +0200

    Update definitions.h

--- src/Modules/definitions.h
+++ src/Modules/definitions.h
@@ -1,14 +1,16 @@
 #ifndef __DEFINITIONS_H_
 #define __DEFINITIONS_H_
 
-typedef unsigned int u32int;
-typedef signed int s32int;
-typedef unsigned int uint16_t;
-typedef unsigned short u16int;
-typedef signed short s16int;
+#define NULL 0
+typedef long long int64_t;
+typedef unsigned long long uint64_t;
+typedef int int32_t;
+typedef unsigned int uint32_t;
+typedef short int16_t;
+typedef unsigned short uint16_t;
+typedef char int8_t;
 typedef unsigned char uint8_t;
-typedef unsigned char u8int;
-typedef signed char s8int;
+typedef unsigned int size_t;
 typedef enum { false = 0, true = 1 } bool;
 
 #endif
