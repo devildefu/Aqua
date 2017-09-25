0488f839663a695cabf03ddd2da35f9011948f8e
Grzegorz Kociołek <georgekociolek@gmail.com>
2017-09-25 23:49:12 +0200

    Update lib.h

--- src/Modules/lib.h
+++ src/Modules/lib.h
@@ -4,12 +4,12 @@
 void atoi(char *str, int* a);
 
 ///SYS////
-struct bits {
+#define memSize_b 10240
+#define memPos 0x1010
+ extern struct _byte_ {
   char _1:1, _2:1, _3:1, _4:1,
        _5:1, _6:1, _7:1, _8:1;
-};
-
-extern struct bits mall_mem[100];
+} mall_memory[memSize_b / 8]; 
 
 void* malloc(unsigned size); //<- Not now, maybe tomorrow
 void free(void* ptr); //<- Not now, maybe tomorrow
