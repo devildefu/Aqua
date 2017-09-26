26c4dc99961149189765b91b16f81eba8f6ed946
Grzegorz Kociołek <georgekociolek@gmail.com>
2017-09-26 23:49:41 +0200

    Update lib.h

--- src/Modules/lib.h
+++ src/Modules/lib.h
@@ -5,8 +5,7 @@
 void atoi(char *str, int* a);
 
 ///SYS////
-
- extern struct _byte_ {
+extern struct _byte_ {
   char _1:1, _2:1, _3:1, _4:1,
        _5:1, _6:1, _7:1, _8:1;
 } mall_memory[memShortSize]; 
