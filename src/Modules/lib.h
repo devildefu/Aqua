b791587ed6a1e1c3992b805d59cd27e1b187fde7
Grzegorz Kociołek <georgekociolek@gmail.com>
2017-09-27 17:43:48 +0200

    Update lib.h

--- src/Modules/lib.h
+++ src/Modules/lib.h
@@ -7,18 +7,29 @@ void atoi(char *str, int* a);
 ///SYS////
 extern char memory_allocated[memShortSize];
 
+///////////////////////////
+//BIT CONTROL
+
 int check_bit(char* _byte_);
 void set_bit(char* _byte_, unsigned char bit);
+
+////////////////////////////
+//BASIC RAM CONTROL
 
 void* malloc(unsigned size); //<- Not now, maybe tomorrow
 void free(void* ptr); //<- Not now, maybe tomorrow
 
+/////////////////////////////
+//MEMORY CONTROL
+
 int memcmp(const void* a, const void* b, unsigned int size);
 void* memcpy(void* destination, const void* from, unsigned int block_size);
 
+//////////////////////////////
+//CLOCKS AND TIME
+
 void sleep(int ms); //<- Waits until time
 int clock(); //<- Returns processor clock
-void timer(int ms, void(*func)()); //<- Waits some time and calls function
 
 
 #endif
