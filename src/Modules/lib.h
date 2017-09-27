795785cc0375b31526faf9c9b5a854888ddb3ecb
Grzegorz Kociołek <georgekociolek@gmail.com>
2017-09-27 17:34:03 +0200

    Update lib.h

--- src/Modules/lib.h
+++ src/Modules/lib.h
@@ -5,13 +5,10 @@
 void atoi(char *str, int* a);
 
 ///SYS////
-extern struct _byte_ {
-  char _1:1, _2:1, _3:1, _4:1,
-       _5:1, _6:1, _7:1, _8:1;
-} mall_memory[memShortSize]; 
+extern char memory_allocated[memShortSize];
 
-inline void blockBit(size_t pos, size_t bit);
-inline uint32_t findBit(size_t pos, char block);
+int check_bit(char* _byte_);
+void set_bit(char* _byte_, unsigned char bit);
 
 void* malloc(unsigned size); //<- Not now, maybe tomorrow
 void free(void* ptr); //<- Not now, maybe tomorrow
@@ -19,7 +16,9 @@ void free(void* ptr); //<- Not now, maybe tomorrow
 int memcmp(const void* a, const void* b, unsigned int size);
 void* memcpy(void* destination, const void* from, unsigned int block_size);
 
-void tsleep(int ms); //<- waits some time
+void sleep(int ms); //<- Waits until time
+int clock(); //<- Returns processor clock
+void timer(int ms, void(*func)()); //<- Waits some time and calls function
 
 
 #endif
