9f32ab1166eeba1ccb829ec965fda4e5ebc2b835
Grzegorz Kociołek <georgekociolek@gmail.com>
2017-09-27 17:54:26 +0200

    Update lib.c

--- src/Modules/lib.c
+++ src/Modules/lib.c
@@ -1,18 +1,6 @@
 #include "lib.h"
 #include "definitions.h"
 
-void atoi(char *str, int* a)
-{
-    #include "vga.h"
-    int k = 0;
-    while(*str)
-    {
-        k = (k<<3)+(k<<1)+(*str)-'0';
-        str++;
-    }
-    *a = k;
-}
-
 //////////
 ///SYS////
 
