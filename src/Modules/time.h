33370c33e51a0b1197a6f77f722671aae229479c
Grzegorz Kociołek <georgekociolek@gmail.com>
2017-10-03 21:00:27 +0200

    Update time.h

--- src/Modules/time.h
+++ src/Modules/time.h
@@ -6,6 +6,13 @@
 #define ClocksPerDecisecond ClocksPerMilisecond * 10
 #define ClocksPerCentisecond ClocksPerMilisecond * 100
 #define ClocksPerSecond ClocksPerMilisecond * 1000
+#define ClocksPerMinute ClocksPerSecond * 60
+#define ClocksPerHour ClocksPerMinute * 60
+#define ClocksPerDay ClocksPerHour * 24
+#define ClocksPerWeek ClocksPerDay * 7
+#define ClocksPerMonth ClocksPerDay * 31
+#define ClocksPerYearQuater ClocksPerMonth * 4
+#define ClocksPerYear ClocksPerDay * 365
 
 
 static inline uint64_t clock() {
