676ae0c09187f38eb8245ae957cc8a0b269ae0ff
Grzegorz Kociołek <georgekociolek@gmail.com>
2017-10-04 20:13:19 +0200

    Update string.c

--- src/Modules/string.c
+++ src/Modules/string.c
@@ -1,1 +1,14 @@
+#include "string.h"
+
+void atoi(char *str, int* a)
+{
+    #include "vga.h"
+    int k = 0;
+    while(*str)
+    {
+        k = (k<<3)+(k<<1)+(*str)-'0';
+        str++;
+    }
+    *a = k;
+}
 
