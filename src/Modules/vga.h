2b00bc912b6e22e906e3b2a35fcfb9848334730f
Grzegorz Kociołek <georgekociolek@gmail.com>
2017-10-05 22:44:11 +0200

    Update vga.h

--- src/Modules/vga.h
+++ src/Modules/vga.h
@@ -8,5 +8,7 @@ void putchar(char character);
 void puts(const char* str);
 void color(uint16_t color);
 void gotoxy(int x, int y);
+int sprintf(const char*, ...); //<- Draws on default display buffer text with converted variables
+                               //On success returns size of displayed string
 
 #endif
