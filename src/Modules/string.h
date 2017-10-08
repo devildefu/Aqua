c39b9ae7ef6a18ef7239f74c79cbe149ef6e2607
Grzegorz Kociołek <georgekociolek@gmail.com>
2017-10-08 17:19:07 +0200

    Update string.h

--- src/Modules/string.h
+++ src/Modules/string.h
@@ -5,7 +5,7 @@ void atoi(char *str, int* a);
 
 //void d2s(double n, char* string, size_t size, char dot) //<- Converts number to string
 
-int getStringn(char* ptr, unsigned int ptr_size); //<- Does not use malloc. Adds chars up to size(limit)
-int getStringc(char* ptr, unsigned int ptr_size, char CHAR); //<- Does not use malloc. Adds chars up to limit until char is the same as given
+char* getStringn(char* ptr, unsigned int ptr_size); //<- Does not use malloc. Adds chars up to size(limit). Returns ptr
+char* getStringc(char* ptr, unsigned int ptr_size, char CHAR); //<- Does not use malloc. Adds chars up to limit until char is the same as given. Returns ptr;
 
 int strcmp(const char* a, const char* b); //<- Compares two strings. Returns 0 if all strings is the same;
