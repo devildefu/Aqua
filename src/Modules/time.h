cf5822eb6bc020bec14d2a87cebe9374e563cb62
Grzegorz Kociołek <georgekociolek@gmail.com>
2017-10-08 21:17:40 +0200

    Update time.h

--- src/Modules/time.h
+++ src/Modules/time.h
@@ -2,7 +2,7 @@
 #define TIME_H
 
 #include "definitions.h"
-#define ClocksPerMilisecond 1
+#define ClocksPerMilisecond 3504055.289
 #define ClocksPerDecisecond ClocksPerMilisecond * 10
 #define ClocksPerCentisecond ClocksPerMilisecond * 100
 #define ClocksPerSecond ClocksPerMilisecond * 1000
@@ -13,6 +13,7 @@
 #define ClocksPerMonth ClocksPerDay * 31
 #define ClocksPerYearQuater ClocksPerMonth * 4
 #define ClocksPerYear ClocksPerDay * 365
+#define GHz ClocksPerSecond/100000000
 
 struct {
     uint64_t start;
@@ -30,7 +31,7 @@ static inline int64_t clock() {
 
 static void wait(uint64_t ms) {
     int64_t now = clock();
-    int64_t end = now+ms;
+    int64_t end = now+(float)ms*ClocksPerMilisecond;
    while(clock() <= end) {}
 }
 
