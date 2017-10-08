1b60f5cff88a15269300478ff01e2340ff6e1cfe
Grzegorz Kociołek <georgekociolek@gmail.com>
2017-10-08 14:29:45 +0200

    Update build.py

--- build.py
+++ build.py
@@ -4,9 +4,10 @@ commands = [
     "nasm -felf32 src/boot.asm -o build/boot.o",
     "gcc -m32 -c src/Modules/vga.c -o build/vga.o -std=gnu99 -O2 -Wall -Wextra -fno-builtin -nostdlib",
     "gcc -m32 -c src/kernel.c -o build/kernel.o -std=gnu99 -O2 -Wall -Wextra -fno-builtin -nostdlib",
-    "gcc -m32 -c src/Modules/lib.c -o build/lib.o -std=gnu99 -O2 -Wall -Wextra -fno-builtin -nostdlib",
+    "gcc -m32 -c src/Modules/memory.c -o build/memory.o -std=gnu99 -O2 -Wall -Wextra -fno-builtin -nostdlib",
     "gcc -m32 -c src/Modules/keyboard.c -o build/keyboard.o -std=gnu99 -O2 -Wall -Wextra -fno-builtin -nostdlib",
-    "ld -m elf_i386 build/boot.o build/kernel.o build/vga.o build/keyboard.o build/io.o build/lib.o -T linker.ld -o kernel -O2 -nostdlib"
+    "gcc -m32 -c src/Modules/string.c -o build/string.o -std=gnu99 -O2 -Wall -Wextra -fno-builtin -nostdlib",
+    "ld -m elf_i386 build/boot.o build/kernel.o build/vga.o build/keyboard.o build/io.o build/memory.o build/string.o -T linker.ld -o kernel -O2 -nostdlib"
 ]
 
 for cmd in commands:
