b91d735a41e3e3bf886c12bdb615e5d59a27c6a1
Grzegorz Kociołek <georgekociolek@gmail.com>
2017-10-09 23:12:55 +0200

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -1,34 +1,13 @@
 #include "Modules/vga.h"
 #include "Modules/keyboard.h"
 #include "Modules/string.h"
-//#include "Modules/time.h"
+#include "Modules/time.h"
 
-struct {
-    uint64_t start;
-}   start_clocks;
 
-static inline uint64_t rdtsc() {
-  	uint32_t low, high;
-  	asm volatile ("rdtsc" : "=a"(low), "=d"(high));
-  	return ((uint64_t)high << 32) | low;
-}
-
-static inline int64_t clock() {
-    return rdtsc() - start_clocks.start;
-}
-
-static void wait(uint64_t ms) {
-    int64_t now = clock();
-    int64_t end = now+ms;
-   while(clock() <= end) {}
-}
-
-static inline void initTimer() {
-    start_clocks.start = rdtsc();
-}
 void kmain(void) {
-	initTimer();
-	wait(1000);
-	putchar('a');
+	char str[10];
+	getStringc(str,10,13);
+	memset(str,'-',10);
+	puts(str);
 	return;
 }
