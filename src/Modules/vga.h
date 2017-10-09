9903d8eabc09442d039bffa8bc96277f30ac1cdd
Grzegorz Kociołek <georgekociolek@gmail.com>
2017-10-09 22:18:22 +0200

    Update vga.h

--- src/Modules/vga.h
+++ src/Modules/vga.h
@@ -11,7 +11,8 @@ void clear();
 void putchar(char character);
 void puts(const char* str);
 void color(uint16_t color);
-void gotoxy(int x, int y);
+void sgotoxy(uint16_t x, uint16_t y);
+void mgotoxy(uint16_t xy);
 
 int snprintf(char* s, size_t n, const char* format, ...); //<- Prints formatted text to the string with limitted size
 int sprintf(char* s, const char* format, ...); //<- Prints formatted text to the string
