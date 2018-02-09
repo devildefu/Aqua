d58d41bfbacc2a13d4085da92c9a28d66336bbf8
Grzegorz Kociołek <georgekociolek@gmail.com>
2018-02-09 21:33:49 +0100

    Update vga.h

--- kernel/Include/vga.h
+++ kernel/Include/vga.h
@@ -19,6 +19,9 @@ void sgotoxy(uint16_t x, uint16_t y);
 void mgotoxy(uint16_t xy);
 void newLine();
 void move(uint16_t x, uint16_t y);
+void setPosition(uint16_t x, uint16_t y);
+void setColor(uint16_t x, uint16_t y, uint16_t color);
+void setChar(uint16_t x, uint16_t y, char c);
 
 int snprintf(char* s, size_t n, const char* format, ...); //<- Prints formatted text to the string with limitted size
 int sprintf(char* s, const char* format, ...); //<- Prints formatted text to the string
