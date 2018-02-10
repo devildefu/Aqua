e21e11fe62809c1372b2accb3a3dd55d0e693521
Grzegorz Kociołek <georgekociolek@gmail.com>
2018-02-10 11:37:38 +0100

    Update keyboard.h

--- kernel/Include/keyboard.h
+++ kernel/Include/keyboard.h
@@ -1,13 +1,9 @@
 #ifndef __KEYBOARD_H_
 #define __KEYBOARD_H_
 
-typedef enum {
-	GET_WAIT,
-	GET_NOWAIT
-} BREAK;
 
 char getKeycode();
 char getKeycodeWait();
-char getchar(BREAK show_opts);
+char getchar(char show_opts);
 
 #endif
