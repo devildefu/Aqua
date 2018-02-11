7ff0d4cefebf62cf4bc474743fefeec68f59231e
PanCzerwonySer <panczerwonyser@gmail.com>
2018-02-11 19:14:39 +0100

    Kirid 0.1 Hydrogen

--- script.sh
+++
@@ -1,8 +0,0 @@
-if grub-file --is-x86-multiboot kirid.bin; then
-  echo multiboot confirmed
-  rm kirid.iso
-  cp -f kirid.bin isodir/boot/kirid.bin
-  grub-mkrescue -o kirid.iso isodir
-else
-  echo the file is not multiboot
-fi
