9af4e908be58b12313f984b6005cc2dda93eb045
Devourer of Source <panczerwonyser@gmail.com>
2018-05-15 19:12:08 +0200

    Update v86.asm

--- src/kernel/v86.asm
+++ src/kernel/v86.asm
@@ -12,7 +12,7 @@ entering_v86:
   push dword [ebp+4]         ; ss
   push dword [ebp+8]         ; esp
   pushfd                     ; eflags
-  or dword [esp], (1 << 17)
-  push dword [ebp+12]
-  push dword [ebp+16]
+  or dword [esp], (1 << 17)  ; set VM flags
+  push dword [ebp+12]        ; cs
+  push dword [ebp+16]        ; eip
   iret
