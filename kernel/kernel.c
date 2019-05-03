807f541d4328467afef0b63eb25611f3050525f5
Devourer of Source <panczerwonyser@gmail.com>
2019-05-03 18:31:20 +0200

    debug

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -3,10 +3,18 @@
 #include <video/tty.h>
 #include <video/colors.h>
 #include <multiboot/multiboot.h>
+#include <devices/serial.h>
+
+#include <debug/debug.h>
 
 void kmain(unsigned long magic, const void* multiboot_pointer) {
     clear();
     color(7);
+
+    /* Initialize devices */
+    com_init(COM1_PORT);
+
     printf("Aqua %s version %s", SYSTEM_ARCH, SYSTEM_VERSION);
+    debug("aaa");
     return;
 }
---
+++ sdk/c/debug/debug.h
@@ -0,0 +1,8 @@
+#ifndef __DEBUG_H_
+#define __DEBUG_H_
+
+#include <devices/serial.h>
+
+#define debug(msg) com_print(COM1_PORT, msg)
+
+#endif
