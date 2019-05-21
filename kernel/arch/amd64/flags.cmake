70147c27c9a00bf790c4e5828ff0c970718f7fc7
Devourer of Source <panczerwonyser@gmail.com>
2019-05-21 18:33:17 +0200

    Removed amd64 from arch directory

--- kernel/arch/amd64/flags.cmake
+++
@@ -1,3 +0,0 @@
-set(ARCH_LINKER_FLAGS "-ffreestanding -O2 -nostdlib -lgcc")
-set(ARCH_C_FLAGS "-m64 -ffreestanding -z max-page-size=0x1000 -mno-red-zone -mno-mmx -mno-sse -mno-sse2 -std=gnu11 -O2 -nostdlib")
-set(ARCH_ASM_FLAGS "-felf64")
