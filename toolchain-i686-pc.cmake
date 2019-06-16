d66e163b49b270920f89ec109ad764fe9c5022a3
Devourer of Source <panczerwonyser@gmail.com>
2019-06-16 18:21:01 +0200

    Fixed the cmake problems
    - Compilers have been forced in a new way
    - Fixed the linker change

--- toolchain-i686-pc.cmake
+++ toolchain-i686-pc.cmake
@@ -10,9 +10,12 @@ set(SYSTEM_PLATFORM "pc")
 # Empty flags variables
 set(CMAKE_EXE_LINKER_FLAGS "")
 set(CMAKE_C_FLAGS "")
-set(CMAKE_CXX_FLAGS "")
 
 # Force compilers
 set(CMAKE_ASM_NASM_COMPILER nasm)
 set(CMAKE_C_COMPILER i686-elf-gcc)
-set(CMAKE_LINKER i686-elf-ld)
+set(LINKER "i686-elf-ld") # I don't know why, but if we set up CMAKE_LINKER, it's empty...
+
+# Inform the cmake that everything is all right.
+set(CMAKE_C_COMPILER_WORKS 1)
+set(CMAKE_CXX_COMPILER_WORKS 1) # Funny, because we don't use i686-elf-c++ but CMake still checks it out...
