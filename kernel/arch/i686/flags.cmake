5c4efdbf227db4468cb3d25074ebdfef675c624a
Devourer of Source <panczerwonyser@gmail.com>
2019-06-17 18:51:32 +0200

    Files moved and cmake improved
    - The i686-only files are in a separate folder
    - There are separate cmake files in the arch folder to add files

--- kernel/arch/i686/flags.cmake
+++
@@ -1,3 +0,0 @@
-set(ARCH_LINKER_FLAGS "-m elf_i386 -O0 -nostdlib")
-set(ARCH_C_FLAGS "-std=gnu11 -ffreestanding -m32 -fno-builtin -nostdlib -fstack-protector")
-set(ARCH_ASM_FLAGS "-felf32")
---
+++ kernel/arch/i686/settings.cmake
@@ -0,0 +1,11 @@
+set(ARCH_LINKER_FLAGS "-m elf_i386 -O0 -nostdlib")
+set(ARCH_C_FLAGS "-std=gnu11 -ffreestanding -m32 -fno-builtin -nostdlib -fstack-protector")
+set(ARCH_ASM_FLAGS "-felf32")
+
+list(APPEND SOURCE  arch/i686/pc/boot.asm
+                    arch/i686/cpu/cpu.c
+                    arch/i686/cpu/gdt.asm
+                    arch/i686/cpu/gdt.c
+                    arch/i686/devices/serial/serial.c
+                    arch/i686/misc/registers.asm
+                    arch/i686/video/tty.c)
